ce4f357401495e8f18618c7c1d0a502d18e2507d
Chen Guo <chen.guo.0625@gmail.com>
2011-05-26 20:26:17 -0700

    Add common.cpp

---
+++ src/common.cpp
@@ -0,0 +1,12 @@
+#include "common.h"
+
+int ins_cyc[] = {
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+};
+
+// Count the number of bits that were turn on from B to A.
+int bits_on (uint32_t a, uint32_t b)
+{
+
+  return 0;
+}
