0d36392577d153bc0938cb9e19714c0b4d596d9c
Chen Guo <chen.guo.0625@gmail.com>
2011-05-28 13:36:18 -0700

    Implement bits_on () in common.cpp.

--- src/common.cpp
+++ src/common.cpp
@@ -7,6 +7,14 @@ int ins_cyc[] = {
 // Count the number of bits that were turn on from B to A.
 int bits_on (uint32_t a, uint32_t b)
 {
+  int bits_on = 0;
+  uint32_t mask = 1;
+  for (int i = 0; i < 32; i++)
+    {
+      if ((a & mask) && !(b & mask))
+        bits_on++;
+      mask = mask << 1;
+    }
 
-  return 0;
+  return bits_on;
 }
