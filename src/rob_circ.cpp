c6f93a6dbd23fd8f5abc6bda5744bcdccffcebcf
Chen Guo <chen.guo.0625@gmail.com>
2011-05-28 13:20:43 -0700

    Separate ROB_Circ.run() into separate functions.
    * src/rob_circ.cpp: (update_entries) New function. Update ROB
    entries.
    (write_to_arf) New function. Commit instructionst to ARF.
    (read_from_iq) New function. Read instructions from dispatch.

--- src/rob_circ.cpp
+++ src/rob_circ.cpp
@@ -24,88 +24,12 @@ void ROB_Circ::run (ins_t ins[])
   // be careful of how variable changes may affect this.
   while (1)
     {
-      uint32_t old_tail = m_tail;
       uint32_t old_head = m_head;
       bool old_empty = m_empty;
 
-      // For each instruction currently in the buffer, decrement
-      // their cycles to complete count.
-      if (!m_empty)
-        {
-          uint32_t i = m_head;
-          do
-            {
-              if (m_buf[i].cycles)
-                {
-                  m_buf[i].cycles--;
-
-                  if (m_buf[i].cycles == 0)
-                    {
-                      cout << "Got result " << i << endl;
-                      m_buf[i].valid = true;
-                      m_nbiton = bits_on (true, false);
-                      m_nrex++;
-                    }
-                }
-              i = (i + 1) % m_size;
-            }
-          while (i != m_tail);
-        }
-
-      // Write instructions to the ARF.
-      if (!m_empty)
-        {
-          uint32_t nwritten = 0;
-          do
-            {
-              // If entry is valid, write it.
-              if (m_buf[m_head].valid)
-                {
-                  cout << "Write from " << m_head << endl;
-                  m_head = (m_head + 1) % m_size;
-                  nwritten++;
-                }
-              else
-                break;
-             }
-          while (m_head != m_tail && nwritten < m_n);
-          m_nwarf += nwritten;
-
-          if (m_head == m_tail && nwritten)
-            m_empty = true;
-        }
-      // Process head pointer.
-      m_nbiton = bits_on (m_head, old_head);
-
-      // Read instructions from issue
-      if ((old_empty || (old_head != m_tail))
-          && ins[ins_num].type != -1)
-        {
-          uint32_t nread = 0;
-          do
-            {
-              cout << "Read into " << m_tail << endl;
-              // Write entry.
-              entry_t old_entry = m_buf[m_tail];
-              m_buf[m_tail].valid = false;
-              m_buf[m_tail].cycles = ins_cyc[ins[ins_num].type];
-              m_buf[m_tail].pc = ins[ins_num].pc;
-              m_buf[m_tail].reg_id = 0;  // TODO: ???
-              ins_num++;
-              nread++;
-
-              // Count bits turned on.
-              m_nbiton += bits_on (m_buf[m_tail].pc, old_entry.pc);
-              m_nbiton += bits_on (m_buf[m_tail].reg_id, old_entry.reg_id);
-              m_tail = (m_tail + 1) % m_size;
-            }
-          while (m_tail != old_head && nread < m_n
-                 && ins[ins_num].type != -1);
-          m_empty = false;
-          m_nriq += nread;
-        }
-      // Process tail pointer.
-      m_nbiton += bits_on (m_tail, old_tail);
+      update_entries ();
+      write_to_arf ();
+      ins_num = read_from_iq (old_head, old_empty, ins_num, ins);
 
       // TODO: Count the read ports being driven for operands.
       // Get percentage this happens from Henry.
@@ -125,4 +49,105 @@ void ROB_Circ::run (ins_t ins[])
     }
 
   cout << "Simulation complete." << endl;
+}
+
+// For each instruction between the head and tail buffers, decrement their
+// cycles to completion count. When it reaches 0, drive the FROM_EX port.
+void ROB_Circ::update_entries ()
+{
+  if (!m_empty)
+    {
+      uint32_t i = m_head;
+      do
+        {
+          if (m_buf[i].cycles)
+            {
+              m_buf[i].cycles--;
+
+              if (m_buf[i].cycles == 0)
+                {
+                  cout << "Got result " << i << endl;
+                  m_buf[i].valid = true;
+                  m_nbiton = bits_on (true, false);
+                  m_nrex++;
+                }
+            }
+          i = (i + 1) % m_size;
+        }
+      while (i != m_tail);
+    }
+}
+
+// From the head pointer, commit up to m_n instructions to the ARF.
+void ROB_Circ::write_to_arf ()
+{
+  uint32_t nwritten = 0;
+  if (!m_empty)
+    {
+      // Loop through entries. Stopping conditions:
+      // 1. m_head == m_tail: buffer is empty
+      // 2. nwritten == m_n: all commit ports used
+      // 3. m_buf[m_head].valid == false: invalid entry
+      do
+        {
+          // Check entry for validity.
+          if (m_buf[m_head].valid)
+            {
+              // If valid, commit it.
+              cout << "Write from " << m_head << endl;
+              m_head = (m_head + 1) % m_size;
+              nwritten++;
+            }
+        }
+      while (m_head != m_tail && nwritten < m_n && m_buf[m_head].valid);
+      // Update writes to ARF count.
+      m_nwarf += nwritten;
+
+      // If buffer is emty now, set the flag.
+      if (m_head == m_tail && nwritten)
+        m_empty = true;
+    }
+  // Process head pointer bit flips.
+  m_nbiton = bits_on (m_head, m_head - nwritten);
+}
+
+// Read instructions from issue, up to m_n instructions.
+int ROB_Circ::read_from_iq (uint32_t old_head, bool old_empty,
+                            int ins_num, ins_t ins[])
+{
+  uint32_t nread = 0;
+
+  // Read instructions from issue, if
+  // 1. Buffer is empty
+  // 2. Buffer is not empty, but head != tail
+  //    (buffer is full if !empty and head == tail)
+  // 3. Instruction sequence isn't over (denoted by -1 type)
+  if ((old_empty || (old_head != m_tail))
+      && ins[ins_num].type != -1)
+    {
+      do
+        {
+          cout << "Read into " << m_tail << endl;
+          // Write entry.
+          entry_t old_entry = m_buf[m_tail];
+          m_buf[m_tail].valid = false;
+          m_buf[m_tail].cycles = ins_cyc[ins[ins_num].type];
+          m_buf[m_tail].pc = ins[ins_num].pc;
+          m_buf[m_tail].reg_id = 0;  // TODO: ???
+          ins_num++;
+          nread++;
+
+          // Count bits turned on.
+          m_nbiton += bits_on (m_buf[m_tail].pc, old_entry.pc);
+          m_nbiton += bits_on (m_buf[m_tail].reg_id, old_entry.reg_id);
+          m_tail = (m_tail + 1) % m_size;
+        }
+      while (m_tail != old_head && nread < m_n && ins[ins_num].type != -1);
+
+      m_empty = false;
+      m_nriq += nread;
+    }
+  // Process tail pointer bit flips.
+  m_nbiton += bits_on (m_tail, m_tail - nread);
+  return ins_num;
 }
